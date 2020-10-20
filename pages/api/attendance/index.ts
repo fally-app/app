56e831225316316d3664f8d9501739d98a8d3bcc
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-20 17:59:36 +0200

    fixed attendances

---
+++ pages/api/attendance/index.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import Attendance from '../../../models/Attendances'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+): Promise<void> {
+    const { method } = req
+
+    switch (method) {
+        case 'GET':
+            try {
+                const attend = await Attendance.find().sort({ created_at: 1 })
+                res.status(200).json({ success: true, data: attend })
+            } catch (error) {
+                res.status(200).json({ error, success: false })
+            }
+            break
+        case 'POST':
+            try {
+                const attendance = new Attendance(req.body)
+                attendance.save()
+            } catch (error) {
+                res.status(200).json({ error, success: false })
+            }
+            break
+        default:
+            res.status(400).json({ success: true })
+            break
+    }
+}

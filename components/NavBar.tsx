2a3532e622ebd039b25ec467579fe20d71474c10
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-20 19:36:21 +0200

    Fixed color inherit error on navba

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -34,7 +34,7 @@ export const NavBar = (): React.ReactElement => {
                     <Typography variant="h6" className={classes.title}>
                         App
                     </Typography>
-                    <Button color="ínherit">Login</Button>
+                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </div>

8dfab27b0506220fea284f8beae5e1c2b32a83a1
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-22 12:35:35 +0200

    added admin

---
+++ pages/admin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NavBar from '../components/NavBar'
+
+export const admin = (): React.ReactElement => {
+    return (
+        <>
+            <NavBar />
+            <h1>Admin here</h1>
+        </>
+    )
+}
+export default admin

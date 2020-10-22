4491f994f405d9a02f29ac76ef95d4d6435df339
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-22 12:04:04 +0200

    finished login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,64 @@
-import { Button, Grid, TextField } from '@material-ui/core'
+import {
+    Button,
+    Card,
+    CardContent,
+    Grid,
+    makeStyles,
+    TextField,
+    Typography,
+} from '@material-ui/core'
 import React from 'react'
 
+const useStyles = makeStyles({
+    root: {
+        width: '100vw',
+        height: '100vh',
+    },
+    blocks: {
+        marginBottom: 25,
+    },
+    header: {
+        textAlign: 'center',
+    },
+})
+
 export const login = (): React.ReactElement => {
+    const classes = useStyles()
     return (
-        <div>
-            <Grid
-                container
-                alignContent="center"
-                direction="column"
-                style={{ minHeight: '100vh', minWidth: '100vw' }}>
-                <TextField label="username" />
-                <TextField label="passowrd" />
-                <Button color="primary" variant="contained">
-                    Login here
-                </Button>
-            </Grid>
-        </div>
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            className={classes.root}
+            alignItems="center">
+            <Card>
+                <CardContent>
+                    <Typography
+                        variant="h4"
+                        component="h2"
+                        className={classes.header}>
+                        Login
+                    </Typography>
+                    <form>
+                        <TextField
+                            label="code"
+                            fullWidth
+                            type="text"
+                            className={classes.blocks}
+                        />
+                        <TextField
+                            label="passowrd"
+                            type="password"
+                            fullWidth
+                            className={classes.blocks}
+                        />
+                        <Button color="primary" variant="contained" fullWidth>
+                            Login here
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
+        </Grid>
     )
 }
 export default login

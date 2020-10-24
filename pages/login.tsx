f335f179c6753fccfe16fbc2cb20c52f9dddfaeb
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-24 16:17:17 +0200

    fixed login issues

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,6 +7,7 @@ import {
     TextField,
     Typography,
 } from '@material-ui/core'
+import Head from 'next/head'
 import React from 'react'
 
 const useStyles = makeStyles({
@@ -25,40 +26,48 @@ const useStyles = makeStyles({
 export const login = (): React.ReactElement => {
     const classes = useStyles()
     return (
-        <Grid
-            container
-            direction="column"
-            justify="center"
-            className={classes.root}
-            alignItems="center">
-            <Card>
-                <CardContent>
-                    <Typography
-                        variant="h4"
-                        component="h2"
-                        className={classes.header}>
-                        Login
-                    </Typography>
-                    <form>
-                        <TextField
-                            label="code"
-                            fullWidth
-                            type="text"
-                            className={classes.blocks}
-                        />
-                        <TextField
-                            label="passowrd"
-                            type="password"
-                            fullWidth
-                            className={classes.blocks}
-                        />
-                        <Button color="primary" variant="contained" fullWidth>
-                            Login here
-                        </Button>
-                    </form>
-                </CardContent>
-            </Card>
-        </Grid>
+        <>
+            <Head>
+                <title>LOGIN - SABBATH SCHOOL</title>
+            </Head>
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                className={classes.root}
+                alignItems="center">
+                <Card>
+                    <CardContent>
+                        <Typography
+                            variant="h4"
+                            component="h2"
+                            className={classes.header}>
+                            Login
+                        </Typography>
+                        <form>
+                            <TextField
+                                label="code"
+                                fullWidth
+                                type="text"
+                                className={classes.blocks}
+                            />
+                            <TextField
+                                label="passowrd"
+                                type="password"
+                                fullWidth
+                                className={classes.blocks}
+                            />
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                fullWidth>
+                                Login here
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </>
     )
 }
 export default login

06514a5167c4243f5ba5f01d2a200d153bdcc4ed
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-10-24 11:09:02 +0200

    add login route

--- utils/Helpers.ts
+++ utils/Helpers.ts
@@ -1,3 +1,7 @@
+import jwt from 'jsonwebtoken'
+
+import User, { IUser } from '../models/User'
+
 export function codeGenerator(person: string): string {
     let code = ''
     let mainString
@@ -15,4 +19,17 @@ export function codeGenerator(person: string): string {
     code += AcdemicYear
 
     return code
+}
+
+export const createToken = ({ id }: { id: string }): string => {
+    return jwt.sign({ id }, process.env.JWT_SECRET)
+}
+
+export const getUserFromToken = async (token: string): Promise<IUser> => {
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        return await User.findById((<IUser>user).id)
+    } catch (error) {
+        return null
+    }
 }

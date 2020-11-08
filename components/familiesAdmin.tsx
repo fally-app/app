06c75f92cba7002fbf2fbb66e6b820648e5e6427
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-11-08 12:34:34 +0200

    fix casinng

--- components/familiesAdmin.tsx
+++ components/familiesAdmin.tsx
@@ -26,6 +26,7 @@ import { IFamilyTypes } from '../models/Family'
 import { IStatus } from '../models/User'
 import UpdateFam from './UpdateFam'
 // import UpdateUser from './UpdateUser'
+console.log("fix it")
 
 interface IFamilyType {
     _id: string

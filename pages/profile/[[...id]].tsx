3b90415ebda48aa5fc92d0fd0d9757e541a01927
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-08 14:22:59 +0200

    updated profile page

--- pages/profile/[[...id]].tsx
+++ pages/profile/[[...id]].tsx
@@ -1,7 +1,19 @@
+import Head from 'next/head'
 import React from 'react'
 
+import NavBar from '../../components/NavBar'
+
 const Profile: React.FC = (): React.ReactElement => {
-    return <div>Profile page here</div>
+    return (
+        <>
+            <Head>
+                <title>Profile Page</title>
+            </Head>
+            <div>
+                <NavBar />
+            </div>
+        </>
+    )
 }
 
 export default Profile

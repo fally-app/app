dacffa1f3606ccc9e797a656236a5811ff9eefa2
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-27 13:16:21 +0200

    navigation bar done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 // import { Button, Typography } from '@material-ui/core'
 import Head from 'next/head'
-// import Link from 'next/link'
+import Link from 'next/link'
 import Router from 'next/router'
 import { useEffect } from 'react'
 
@@ -36,6 +36,46 @@ const App: React.FC = (): React.ReactElement => {
                         Login here
                     </Button>
                 </Link> */}
+                <nav className=" flex justify-around pt-4">
+                    <h3 className="text-white text-3xl font-bold">SDA</h3>
+                    <div className=" flex justify-between gap-8 text-white">
+                        <Link href="/schedule">
+                            <a>Schedule</a>
+                        </Link>
+                        <Link href="/blog">
+                            <a>Blog</a>
+                        </Link>
+                        <Link href="/signin">
+                            <a>Signin</a>
+                        </Link>
+                        <Link href="/search">
+                            <a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    width="24"
+                                    height="24"
+                                    className="fill-current">
+                                    <path fill="none" d="M0 0h24v24H0z" />
+                                    <path d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z" />
+                                </svg>
+                            </a>
+                        </Link>
+                        <Link href="/more">
+                            <a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    width="24"
+                                    height="24"
+                                    className="fill-current">
+                                    <path fill="none" d="M0 0h24v24H0z" />
+                                    <path d="M3 4h18v2H3V4zm6 7h12v2H9v-2zm-6 7h18v2H3v-2z" />
+                                </svg>
+                            </a>
+                        </Link>
+                    </div>
+                </nav>
             </div>
         </>
     )

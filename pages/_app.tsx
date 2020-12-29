6365639a24c9c301fc30fe27952326d20c783b90
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-29 15:18:25 +0200

    working more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,6 @@ export default function MyApp(props: AppProps): React.ReactElement {
     const { Component, pageProps } = props
 
     React.useEffect(() => {
-        // Remove the server-side injected CSS.
         const jssStyles = document.querySelector('#jss-server-side')
         if (jssStyles) {
             jssStyles.parentElement?.removeChild(jssStyles)

8aa417bcd9c2dc0a1aa7ed1af7f8ccddd70fad35
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-03 13:33:26 +0200

    update on auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,5 @@
 export default async (req, res, next) => {
-    const token =
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
+    const token = req.headers.authorization.split(' ')[1]
 
     if (token) {
         req.token = token

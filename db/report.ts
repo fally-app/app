b77a63cccc9a481595ff34753c379972aec5c17e
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-03 12:51:22 +0200

    adding match on the query

--- db/report.ts
+++ db/report.ts
@@ -1,18 +1,23 @@
 import { Db } from 'mongodb'
 
+import { getCurrentWeekInTheYear } from '../utils/Helpers'
+
 export const getCurrentReport = async (db: Db) => {
-    return db
-        .collection('familyreports')
-        .aggregate([
-            {
-                $lookup: {
-                    from: 'families',
-                    localField: 'family',
-                    foreignField: '_id',
-                    as: 'family',
-                },
+    return db.collection('familyreports').aggregate([
+        {
+            $lookup: {
+                from: 'families',
+                localField: 'family',
+                foreignField: '_id',
+                as: 'family',
             },
-        ])
-        .sort({ percentage: -1 })
-        .toArray()
+        },
+        {
+            $match: {
+                sabbath_week: getCurrentWeekInTheYear()[1],
+                year: getCurrentWeekInTheYear()[0],
+            },
+        },
+        { $sort: { percentage: -1 } },
+    ])
 }

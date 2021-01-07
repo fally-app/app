b7e9d665bbbfbf32ea021452c3848657505614a9
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-07 22:06:04 +0200

    added get users handler

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,61 +1,70 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import nc from 'next-connect'
 
-import { connectToDB, user } from '../../../db'
-import User from '../../../models/User'
-import connectDB from '../../../utils/connectDB'
-import { codeGenerator } from '../../../utils/Helpers'
+import { user } from '../../../db'
+import middleware from '../../../middleware/all'
+import onError from '../../../middleware/error'
+// import { codeGenerator } from '../../../utils/Helpers'
+import { Request } from '../../../utils/types'
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-): Promise<void> {
-    await connectDB()
-    const { method } = req
+const handler = nc({ onError })
 
-    switch (method) {
-        case 'GET':
-            try {
-                const { db } = await connectToDB()
-                const users = await user.getAllUsers(db)
-                res.status(200).send(users)
-            } catch (error) {
-                res.status(400).json({ success: false, error })
-            }
-            break
-        case 'POST':
-            try {
-                const {
-                    firstName,
-                    lastName,
-                    gender,
-                    email,
-                    family_id,
-                    class_level,
-                } = req.body
+handler.use(middleware)
 
-                if (!firstName || !lastName || !family_id || !class_level)
-                    return res.status(400).json({
-                        success: false,
-                        error: 'All fields are required',
-                    })
+handler.get(async (req: Request, res) => {
+    const users = await user.getAllUsers(req.db)
+    res.send({ data: users })
+})
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ): Promise<void> {
+//     await connectDB()
+//     const { method } = req
 
-                const users = new User({
-                    firstName,
-                    lastName,
-                    gender,
-                    email,
-                    family_id,
-                    class_level,
-                })
-                users.code = codeGenerator('user')
-                await users.save()
-                res.status(200).json({ success: true, data: users })
-            } catch (error) {
-                res.status(400).json({ success: false, error })
-            }
-            break
-        default:
-            res.status(404).json({ success: false })
-            break
-    }
-}
+//     switch (method) {
+//         case 'GET':
+//             try {
+//                 const { db } = await connectToDB()
+//                 const users = await user.getAllUsers(db)
+//                 res.status(200).send(users)
+//             } catch (error) {
+//                 res.status(400).json({ success: false, error })
+//             }
+//             break
+//         case 'POST':
+//             try {
+//                 const {
+//                     firstName,
+//                     lastName,
+//                     gender,
+//                     email,
+//                     family_id,
+//                     class_level,
+//                 } = req.body
+
+//                 if (!firstName || !lastName || !family_id || !class_level)
+//                     return res.status(400).json({
+//                         success: false,
+//                         error: 'All fields are required',
+//                     })
+
+//                 const users = new User({
+//                     firstName,
+//                     lastName,
+//                     gender,
+//                     email,
+//                     family_id,
+//                     class_level,
+//                 })
+//                 users.code = codeGenerator('user')
+//                 await users.save()
+//                 res.status(200).json({ success: true, data: users })
+//             } catch (error) {
+//                 res.status(400).json({ success: false, error })
+//             }
+//             break
+//         default:
+//             res.status(404).json({ success: false })
+//             break
+//     }
+// }

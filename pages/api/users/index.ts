66d8ac9cfb1e331edb192856b1c4bf408c9ac5a5
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-09 13:20:24 +0200

    removed un used codes

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -3,7 +3,6 @@ import nc from 'next-connect'
 import { user } from '@/db/index'
 import middleware from '@/middleware/all'
 import onError from '@/middleware/error'
-// import { codeGenerator } from '../../../utils/Helpers'
 import { Request } from '@/utils/types'
 
 const handler = nc({ onError })
@@ -21,57 +20,3 @@ handler.post(async (req: Request, res) => {
 })
 
 export default handler
-// export default async function handler(
-//     req: NextApiRequest,
-//     res: NextApiResponse
-// ): Promise<void> {
-//     await connectDB()
-//     const { method } = req
-
-//     switch (method) {
-//         case 'GET':
-//             try {
-//                 const { db } = await connectToDB()
-//                 const users = await user.getAllUsers(db)
-//                 res.status(200).send(users)
-//             } catch (error) {
-//                 res.status(400).json({ success: false, error })
-//             }
-//             break
-//         case 'POST':
-//             try {
-//                 const {
-//                     firstName,
-//                     lastName,
-//                     gender,
-//                     email,
-//                     family_id,
-//                     class_level,
-//                 } = req.body
-
-//                 if (!firstName || !lastName || !family_id || !class_level)
-//                     return res.status(400).json({
-//                         success: false,
-//                         error: 'All fields are required',
-//                     })
-
-//                 const users = new User({
-//                     firstName,
-//                     lastName,
-//                     gender,
-//                     email,
-//                     family_id,
-//                     class_level,
-//                 })
-//                 users.code = codeGenerator('user')
-//                 await users.save()
-//                 res.status(200).json({ success: true, data: users })
-//             } catch (error) {
-//                 res.status(400).json({ success: false, error })
-//             }
-//             break
-//         default:
-//             res.status(404).json({ success: false })
-//             break
-//     }
-// }

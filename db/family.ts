8dde4b2acc6c042d831d1c690f2ca1098de8c61b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-09 16:42:22 +0200

    added auth route

--- db/family.ts
+++ db/family.ts
@@ -3,3 +3,7 @@ import { Db } from 'mongodb'
 export const getFamilies = async (db: Db) => {
     return db.collection('families').find().toArray()
 }
+
+export const getFamilyByCode = async (db: Db, code: string) => {
+    return db.collection('families').findOne({ code })
+}

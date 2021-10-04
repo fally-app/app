843349810cbcb4d26ab45fef0f2e1ee58f0a1318
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-10-04 22:34:33 +0200

    chore: start migration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,7 @@
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { ThemeProvider } from '@material-ui/core/styles'
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import React from 'react'
-import '../styles/tailwind.css'
-import theme from '../theme'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function MyApp(props: AppProps): React.ReactElement {
-    const { Component, pageProps } = props
-    React.useEffect(() => {
-        const jssStyles = document.querySelector('#jss-server-side')
-        if (jssStyles) {
-            jssStyles.parentElement?.removeChild(jssStyles)
-        }
-    }, [])
-
-    return (
-        <React.Fragment>
-            <Head>
-                <title>Welcome</title>
-            </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </React.Fragment>
-    )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+export default MyApp
